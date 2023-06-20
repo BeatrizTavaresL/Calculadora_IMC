2021f5b544b24f6c2d93367c05d2d61e75ed199f
BeatrizTavaresL <beatriz.tavares18@icloud.com>
2023-06-20 20:14:07 -0300

    reestruturação de pastas e correção de bugs'

--- html/assets/Js/script.js
+++ html/assets/Js/script.js
@@ -4,36 +4,36 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const weight = document.getElementById('weight').value;
-  const height = document.getElementById('height').value;
+  const height = document.getElementById('height').value.replace("," , ".");
 
-  const bm1 = (weight / (height * height)).toFixed(2);
+  const bmi = (weight / (height * height)).toFixed(2);
 
   const value = document.getElementById('value')
   let description = '';
 
-  value.classList.add('attention');
-  
   document.getElementById('infos').classList.remove('hidden');
 
-
-if (bm1 < 18.5) {
+     console.log(bmi)
+if (bmi < 18.5) {
      description = "Cuidado! Você está abaixo do peso!"
 } else if ( bmi >= 18.5 && bmi <= 25.0) {
      description = "Você está no peso ideal!";  
-     value.classList.add('attention');
-     value.classList.add('normal');
 } else if (bmi > 25 && bmi <= 30) {
      description = "Cuidado! Você está sobrepeso!";
+     value.classList.add('attention');
 } else if (bmi > 30 && bm1 <= 35) {
      description = "Cuidado! Você está com obesidade moderada!"; 
+     value.classList.add('attention');
 } else if (bmi > 35 && bmi <= 40) {
      description = "Cuidado! Você está com obesidade severa!"; 
+     value.classList.add('attention');
 } else {
-     description = "Cuidado! Você está com obesidade morbid!"; 
+     description = "Cuidado! Você está com obesidade morbida!"; 
+     value.classList.add('attention');
 }
 
 
-value.textContent = bmi.replace('.', ',');
+value.textContent = bmi
 document.getElementById('description').textContent = description;
 
 });
